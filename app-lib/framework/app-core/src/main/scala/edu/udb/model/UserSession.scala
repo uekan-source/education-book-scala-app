94f33697eba99849639ad5a71fe53544095cf6e4
uekan-source <is.hrsrs05@gmail.com>
2026-07-22 11:02:06 +0900

    Update UserSession.scala

--- app-lib/framework/app-core/src/main/scala/edu/udb/model/UserSession.scala
+++ app-lib/framework/app-core/src/main/scala/edu/udb/model/UserSession.scala
@@ -3,6 +3,7 @@
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
+ *　Userセッションエンティティモデル
  */
 
 package edu.udb.model
