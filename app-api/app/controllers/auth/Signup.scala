8d4d7562d4f8107a163db1ecce749e1adf9d70fe
uekan-source <is.hrsrs05@gmail.com>
2026-07-25 21:59:52 +0900

    Update Signup.scala

--- app-api/app/controllers/auth/Signup.scala
+++ app-api/app/controllers/auth/Signup.scala
@@ -3,7 +3,7 @@
  *
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
- * サインアップ処理
+ * サインアップ用処理
  */
 
 package controllers.auth
